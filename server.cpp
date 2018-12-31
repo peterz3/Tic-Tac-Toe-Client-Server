913dc4d875633f9478cb79a296c68f93cd36c80f
Peterzin1 <peterzin8880@gmail.com>
2018-12-31 18:46:22 -0500

    got client address to display in ipv6

--- server.cpp
+++ server.cpp
@@ -7,40 +7,67 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <unistd.h>
-using namespace std;
 
 #define BACKLOG 5
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     //only argument needed to pass in to server side is the port
     //number on which the app will be hosted, make sure its over 2000
 
-    if(argc != 2 ){
-        cerr << "please specify a port number greater that 2000" << endl;
+    if (argc != 2)
+    {
+        std::cerr << "please specify a port number greater that 2000" << std::endl;
         exit(0);
     }
-        addrinfo hints, *res;
+    addrinfo hints, *res;
 
     memset(&hints, 0, sizeof(hints));
 
-    hints.ai_family = AF_UNSPEC;  // use IPv4 or IPv6, whichever
+    hints.ai_family = AF_UNSPEC;     // use IPv4 or IPv6, whichever
     hints.ai_socktype = SOCK_STREAM; //connection is run until one side disconnects
     hints.ai_flags = AI_PASSIVE;
 
     int sockfd;
-    if((sockfd = getaddrinfo(NULL, argv[1] , &hints, &res)) != 0){
-       fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(sockfd));
-       //print error if address info returns non-zero
+    if ((sockfd = getaddrinfo(NULL, argv[1], &hints, &res)) != 0)
+    {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(sockfd));
+        //print error if address info returns non-zero
     }
-        //open stream oriented socket with internet address
+    //open stream oriented socket with internet address
     // keep track of the server socket descriptor
     int serverSide = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    if(serverSide < 0)
+    if (serverSide < 0)
     {
-        cerr << "Error establishing the server socket" << endl;
-        exit(0);
+        std::cerr << "Error establishing the server socket" << std::endl;
+        exit(1);
     }
     //bind the socket to local address
-    bind(serverSide, res->ai_addr,res->ai_addrlen);
-    cout << "server side binded " << endl;
+    int bindfd;
+    bindfd = bind(serverSide, res->ai_addr, res->ai_addrlen);
+    if (bindfd != 0)
+    {
+        std::cerr << "Failed to bind" << std::endl;
+        exit(1);
+    }
+    std::cout << "server side binded " << std::endl;
+
+    int listenfd;
+    listenfd = listen(serverSide, BACKLOG);
+    if(listenfd != 0){
+        std::cerr << "couldn't listen from socket" << std::endl;
+    }
+
+    sockaddr_in6 client;
+    socklen_t client_size = sizeof(client);
+    int newSockfd = accept(serverSide, (sockaddr *)&client, &client_size);
+    if(newSockfd < 0)
+    {
+        std::cerr << "Error accepting request from client!" << std::endl;
+        exit(1);
+    }
+    char cli_ip[INET6_ADDRSTRLEN];
+    std::cout << cli_ip << std::endl;
+    const char *ptr_cli_ip = inet_ntop(AF_INET6, &client.sin6_addr, cli_ip, sizeof(cli_ip));
+    std::cout << "recieve connection from(ipv6) " << cli_ip << std::endl;
 }
